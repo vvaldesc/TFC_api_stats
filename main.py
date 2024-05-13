b146b8ce6dffa45cfbf221fcaee1875880df5731
Víctor <daw_victorvaldes@riberadeltajo.es>
2024-05-13 12:51:18 +0200

    Refactor

--- main.py
+++ main.py
@@ -10,22 +10,12 @@ app = Flask(__name__)
 def home():
     return prediction.funcion_saludo()
 
-@app.route('/api/data')
-def get_data():
-    return {'key': 'value', 'hello': 'world'}
-
-@app.route('/api/clients')
-def get_clients():
-    response = requests.get('http://localhost:4321/api/clients/clients')
-    return response.json()
-
 #How much time will it take to the client to get his service?
 @app.route('/api/estimatedtime', methods=['POST'])
 def get_estimated_time():
-    historical = requests.get('http://localhost:4321/api/serviceconsumptions/serviceConsumption')
+    historical = requests.get('http://localhost:4321/api/serviceconsumptions/serviceConsumptions')
     delay = decisiontree.decisionTree(historical.json(), request.json)
     return {'request': delay}
-
 
 if __name__ == '__main__':
     app.run(debug=True)
