21db2249de2b0b130084f2deaba8f98a9cfbe775
vvaldesc <victor.valdescobos@riberadeltajo.es>
2024-06-19 10:34:35 +0200

    flask starter

---
+++ start_flask.ps1
@@ -0,0 +1,14 @@
+# Navega hasta el directorio de tu entorno virtual
+cd .\env
+
+# Activa el entorno virtual
+.\Scripts\activate
+
+# Navega de vuelta al directorio de tu proyecto
+cd ..
+
+# Configura la variable de entorno FLASK_APP
+$env:FLASK_APP = "main.py"
+
+# Inicia la aplicación Flask
+flask run
